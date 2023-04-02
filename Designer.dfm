9ed8439f757ea91d2ad227c40dbe1be654d4e921
Sophie <117984294+sophiemie@users.noreply.github.com>
2023-04-02 21:35:53 +0200

    Drei Datenbank-Verbindungen fuer alle Datenbanken erstellt

--- Designer.dfm
+++ Designer.dfm
@@ -20,15 +20,47 @@ object Form1: TForm1
     Height = 25
     Caption = 'Button1'
     TabOrder = 0
-    OnClick = Button1Click
   end
-  object SQLConnection1: TSQLConnection
-    Left = 232
-    Top = 216
+  object FDConnection1: TFDConnection
+    Params.Strings = (
+      'Database=forms'
+      'User_Name=dbapp'
+      'Password=Yv9i9OqHC8TRlCbE'
+      'DriverID=MySQL')
+    Left = 144
+    Top = 184
   end
-  object SQLDataSet1: TSQLDataSet
-    Params = <>
-    Left = 368
-    Top = 312
+  object FDQuery1: TFDQuery
+    Connection = FDConnection1
+    Left = 240
+    Top = 184
+  end
+  object FDConnection2: TFDConnection
+    Params.Strings = (
+      'Database=wf'
+      'User_Name=dbapp'
+      'Password=Yv9i9OqHC8TRlCbE'
+      'DriverID=MySQL')
+    Left = 144
+    Top = 256
+  end
+  object FDQuery2: TFDQuery
+    Connection = FDConnection2
+    Left = 240
+    Top = 264
+  end
+  object FDConnection3: TFDConnection
+    Params.Strings = (
+      'Database=main_db'
+      'User_Name=dbapp'
+      'Password=Yv9i9OqHC8TRlCbE'
+      'DriverID=MySQL')
+    Left = 136
+    Top = 344
+  end
+  object FDQuery3: TFDQuery
+    Connection = FDConnection3
+    Left = 248
+    Top = 352
   end
 end
--- Designer.pas
+++ Designer.pas
@@ -5,15 +5,21 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.FMTBcd, Data.SqlExpr, Data.DB,
-  Vcl.StdCtrls, DatenbankUnit;  // Datenbank.pas einbinden
+  Vcl.StdCtrls, DatenbankUnit, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
+  FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet, FireDAC.Comp.Client;  // Datenbank.pas einbinden
 
 type
   TForm1 = class(TForm)
-    SQLConnection1: TSQLConnection;
     Button1: TButton;
-    datenBank1: Datenbank;
-    SQLDataSet1: TSQLDataSet; // Fehlermeldung: Keine entsprechende Komponente?
-    procedure Button1Click(Sender: TObject);
+    FDConnection1: TFDConnection;
+    FDQuery1: TFDQuery;
+    FDConnection2: TFDConnection;
+    FDQuery2: TFDQuery;
+    FDConnection3: TFDConnection;
+    FDQuery3: TFDQuery;
   private
     { Private-Deklarationen }
   public
@@ -28,9 +34,4 @@ implementation
 {$R *.dfm}
 
 // Datenbank Verbindung herstellen
-procedure TForm1.Button1Click(Sender: TObject);
-begin
-  datenBank1.stelleVerbindungHer(SQLDataSet1, SQLConnection1);
-end;
-
 end.
